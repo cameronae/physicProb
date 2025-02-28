8e7569acdba796778952bcf351e08a7783a5df9f
cameronae <138916869+cameronae@users.noreply.github.com>
2025-02-27 23:22:02 -0500

    Update problem.js

--- problem.js
+++ problem.js
@@ -1,35 +1,6 @@
-######################################
-#  Setup
-
-$a = random(5,15,1);
-$b = random(3,5,1);
-
-######################################
-#  Main text
-
-BEGIN_TEXT
-A tree is supported by a wire anchored in the ground $a feet from
-its base.  The wire is $b feet longer than the height that it reaches
-on the tree. How long is the wire?
-$PAR
-a) Use the Pythagorean theorem to write a polynomial equation to
-solve to answer the question.  Use the variable \(x\) to represent $BBOLD
-the length of the wire $EBOLD.
-$BR
-Answer:  \{ ans_rule(30) \}
-$PAR
-b)  Solve the equation and give the length of the wire.
-$BR
-Answer:  \{ ans_rule(10) \} 
-END_TEXT
-
-######################################
-#  Answer
-
-Context()->variables->are(s=>'Real');
-
-$ans_a = "$a^2 + (x-$b)^2 = x^2";
-ANS(Picky_equation_cmp($ans_a));
-
-$ans_b = ($a**2+$b**2)/(2*$b);
-ANS(num_and_unit_checker($ans_b, "feet")); 
+ const a = Math.floor(Math.random() * (200 - 100 + 1)) + 100; // Random number between 5 and 15
+        const b = Math.floor(Math.random() * (70 - 30 + 1)) + 30; // Random number between 3 and 5
+        const radians = b * (Math.PI / 180);
+        // Display random values for a and b
+        document.getElementById('velo').textContent = a;
+        document.getElementById('theta').textContent = b;
